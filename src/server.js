c05c73a27ba7aa5bd5c863a25d61c132c8cfe190
Davidson William <davidsonwferreira8@gmail.com>
2024-03-26 21:58:23 -0300

    feat: created the user useCase

--- src/server.js
+++ src/server.js
@@ -4,6 +4,5 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-// app.use(encodeURI());
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
---
+++ src/useCases/createUser/CreateUserUseCase.js
@@ -0,0 +1,31 @@
+import { hash } from "bcrypt";
+
+import { client } from "../../config/prisma/prisma.js";
+
+class CreateUserCase {
+    async execute({ username, password, email }) {
+        const userAlreadyExists = await client.user.findFirst({
+            where: {
+                email: email,
+            },
+        });
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        const passwordHash = await hash(password, 8);
+
+        const user = await client.user.create({
+            data: {
+                username: username,
+                email: email,
+                password: passwordHash,
+            },
+        });
+
+        return user;
+    }
+}
+
+export { CreateUserCase };
